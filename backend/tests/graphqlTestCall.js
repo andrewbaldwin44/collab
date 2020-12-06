66b01d7c10ce4e154153158270c0a6a182758adf
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-12-05 22:01:33 -0500

    Add backend tests

---
+++ backend/tests/graphqlTestCall.js
@@ -0,0 +1,17 @@
+const { graphql } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = require('../schema/schema.root');
+const resolvers = require('../resolvers/resolvers.root');
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+module.exports = async function graphqlTestCall(query, variables) {
+  return graphql(
+    schema,
+    query,
+    undefined,
+    {},
+    variables
+  );
+}
